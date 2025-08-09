2b3c7172536ea1df4cb70fa831ff712cfe204303
AL ZAYAN P <alzayanpoothakkal1156@gmail.com>
2025-08-09 05:43:18 +0530

    Your commit message

---
+++ string.js
@@ -0,0 +1,34 @@
+// let s1="sangeeth";
+// let s2="amal";
+// let s3 ="abhijith";
+// console.log(s1,s2,s3);
+// console.log(s1.length);
+// for(let i=0;i<s1.length;i++){
+//     console.log(s1[i]);
+    
+// }
+let s4 ="JavaScript,\
+ often abbreviated as JS,\
+  is a programming language and core technology of the World Wide Web,\
+   alongside HTML and CSS.\
+    Ninety-nine percent of websites use JavaScript on the client side for webpage behavior. Web browsers have a dedicated JavaScript engine that executes the client code."
+// console.log(s4);
+// let keyword ="client"
+// console.log(s4.search(keyword));
+let keyword = "client";
+let replace="huy";
+// console.log(s4.replace(keyword,replace));
+// let portionstring =s4.slice(-11,-8);
+// let portionstring= s4.substring(10,2);
+let portionstring=s4.substr(3,10);
+console.log(portionstring);
+ let fname= "Sangeeeth";
+ let lname="A";
+//  let fullname=fname + lname;
+let fullname = `${fname} ${lname}`;
+// console.log(fullname);
+// console.log(fullname.toLowerCase());
+console.log(fullname.toUpperCase());
+
+
+
